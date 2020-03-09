292a6439e6a225fd6fd7dcfe93e8bb6a9feaa197
DeanCavaliere <49283626+DynamicDumbA55@users.noreply.github.com>
2020-03-09 16:17:56 -0400

    Update subDriveVM.py

--- subDriveVM.py
+++ subDriveVM.py
@@ -2,6 +2,9 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
+import pandas as pd
+
+f = open('data.csv','w')
 
 # Receives joystick messages (subscribed to Joy topic)
 # then converts the joysick inputs into Twist commands
@@ -9,18 +12,20 @@ from sensor_msgs.msg import Joy
 # axis 0 aka left stick horizonal controls angular speed
 def callback(data):
     twist = Twist()
-    twist.linear.x = data.axes[1]  # Left Veritcal Stick
+    twist.angular.x = data.axes[4]  # Left Veritcal Stick
     twist.angular.z = data.axes[0]  # Left Horizontal Stick
+    df = pd.DataFrame({'Steering' : [twist.angular.z], 'Speed' : [twist.angular.x]})
+    df.to_csv('data.csv',mode='a',header=False,index=False)
     # twist.linear.x = data.axes[4] #Right Vertical Stick
     # twist.angular.z = data.axes[3] #Right Horizontal Stick
     # print(str(Twist))
     # FORWARD/BACK =  1/-1.
-    if float(twist.linear.x) > float(0.500):
-        hold1 = ('Forwards    ' + str(float(twist.linear.x)) + '           ')
-    elif float(twist.linear.x) < float(-0.500):
-        hold1 = ('Backwards   ' + str(float(twist.linear.x)) + '           ')
+    if float(twist.angular.x) > float(0.500):
+        hold1 = ('Forwards    ' + str(float(twist.angular.x)) + '           ')
+    elif float(twist.angular.x) < float(-0.500):
+        hold1 = ('Backwards   ' + str(float(twist.angular.x)) + '           ')
     else:
-        hold1 = ('Stop        ' + str(float(twist.linear.x)) + '           ')
+        hold1 = ('Stop        ' + str(float(twist.angular.x)) + '           ')
     # LEFT/RIGHT = 1/-1
     if float(twist.angular.z) > float(0.500):
         hold2 = ('Left        ' + str(float(twist.angular.z)))
