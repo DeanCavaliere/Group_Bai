86b943dc59b8551d9d4c50ab3d41259998480217
DeanCavaliere <49283626+DynamicDumbA55@users.noreply.github.com>
2020-03-09 15:33:11 -0400

    Update subDrive.py

--- subDrive.py
+++ subDrive.py
@@ -4,46 +4,46 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-#from rpiHAT import ServoNT
+from rpiHAT import ServoNT
 import time
 
-low = 0.12
+lowTurn = 0.11
 mid = 0.15
-high = 0.18
+highTurn = 0.19
+
+lowSpeed = 0.14
+highSpeed = 0.16
 
 
 def callback(data):
     #rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
     twist = Twist()
-    twist.linear.x = data.axes[1]
+    twist.angular.x = data.axes[4]
     twist.angular.z = data.axes[0]
     #print('linear x is: ' + str(twist.linear.x)) #FORWARD/BACK =  1/-1
     #print('Angular z is ' + str(twist.angular.z)) #LEFT/RIGHT = 1/-1
-    
     s=ServoNT(channel=2, freq=97.9) #Steering?
     d=ServoNT(channel=1, freq=97.9) #Driving?
-    
-    if float(twist.linear.x) > float(0.500):
-        hold1 = (float(twist.linear.x)) #Forwards
-        s.pulse(high)
-    elif float(twist.linear.x) < float(-0.500):
-        hold1 = (float(twist.linear.x)) #Backwards
-        s.pulse(low)
+    if twist.angular.x > float(0.250):
+       # hold1 = (float(twist.linear.x)) #Forwards
+        s.pulse(highSpeed)
+    elif (twist.angular.x) < float(-0.250):
+       # hold1 = (float(twist.linear.x)) #Backwards
+        s.pulse(lowSpeed)
     else:
-        hold1 = (float(twist.linear.x)) #STOP
+        #hold1 = (float(twist.linear.x)) #STOP
         s.pulse(mid)
     # LEFT/RIGHT = 1/-1
-    if float(twist.angular.z) > float(0.500):
-        hold2 = (float(twist.angular.z)) #LEFT
-        d.pulse(0.12)
-    elif float(twist.angular.z) < float(-0.500):
-        hold2 = (float(twist.angular.z)) #RIGHT
-        d.pulse(0.18)
+    if (twist.angular.z) > float(0.250):
+       # hold2 = (float(twist.angular.z)) #LEFT
+        d.pulse(lowTurn)
+    elif (twist.angular.z) < float(-0.250):
+       # hold2 = (float(twist.angular.z)) #RIGHT
+        d.pulse(highTurn)
     else:
-        hold2 = (float(twist.angular.z)) #STRAGHT
-        d.pulse(0.15)
-    
-    
+       #hold2 = (float(twist.angular.z)) #STRAGHT
+        d.pulse(mid)
+        #print()
     #d.pulse(dummy2)
     #.15 center .12 left .17 right
 
@@ -52,7 +52,7 @@ def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The                                                                                                                                                           # anonymous=True flag means that rospy will choose a unique                                                                                                                                                        # name for our 'listener' node so that multiple listeners can                                                                                                                                                      # run simultaneously.
     rospy.init_node('FastBai')
-    rospy.Subscriber('joy', Joy, callback)
+    rospy.Subscriber('joy', Joy, callback, queue_size=2)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
