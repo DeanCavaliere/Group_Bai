0fa6f48ec72f931054f0be9a9a9311c60ae106d7
Dean Cavaliere <49283626+DeanCavaliere@users.noreply.github.com>
2020-03-09 16:55:05 -0400

    Update subDriveVM.py

--- subDriveVM.py
+++ subDriveVM.py
@@ -4,28 +4,34 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 import pandas as pd
 
+# This code subscribes to the ROS node 'joy' and then will display
+# the corresponding data to screen. It also generates a CSV file 
+# called data.csv
+
+# Overwrite / create the csv file 'data.csv'
 f = open('data.csv','w')
 
-# Receives joystick messages (subscribed to Joy topic)
-# then converts the joysick inputs into Twist commands
-# axis 1 aka left stick vertical controls linear speed
-# axis 0 aka left stick horizonal controls angular speed
 def callback(data):
     twist = Twist()
-    twist.angular.x = data.axes[4]  # Left Veritcal Stick
+    twist.angular.x = data.axes[4]  # Right Veritcal Stick
     twist.angular.z = data.axes[0]  # Left Horizontal Stick
+    
+    #This code generates a CSV file
     df = pd.DataFrame({'Steering' : [twist.angular.z], 'Speed' : [twist.angular.x]})
     df.to_csv('data.csv',mode='a',header=False,index=False)
     # twist.linear.x = data.axes[4] #Right Vertical Stick
     # twist.angular.z = data.axes[3] #Right Horizontal Stick
     # print(str(Twist))
-    # FORWARD/BACK =  1/-1.
+    
+    # Output direction + float value to terminal
+    # FORWARD/BACK =  1/-1
     if float(twist.angular.x) > float(0.500):
         hold1 = ('Forwards    ' + str(float(twist.angular.x)) + '           ')
     elif float(twist.angular.x) < float(-0.500):
         hold1 = ('Backwards   ' + str(float(twist.angular.x)) + '           ')
     else:
         hold1 = ('Stop        ' + str(float(twist.angular.x)) + '           ')
+        
     # LEFT/RIGHT = 1/-1
     if float(twist.angular.z) > float(0.500):
         hold2 = ('Left        ' + str(float(twist.angular.z)))
@@ -40,7 +46,7 @@ def callback(data):
 def start():
     # subscribed to joystick inputs on topic "joy"
     rospy.Subscriber("joy", Joy, callback)
-    # starts the node
+    # starts the node 'VM'
     rospy.init_node('VM')
     rospy.spin()
 
